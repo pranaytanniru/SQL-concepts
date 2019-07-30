25138c1bcef7b466f741ad0e1f9abd2974b537b4
pranaytanniru <pranaytanniru1132@gmail.com>
2019-07-30 11:07:10 +0530

    added teams problem

---
+++ Problems/teamsProblem.sql
@@ -0,0 +1,45 @@
+Input : Given a table TEAMS
+TEAMS:
+------
+  IND
+  AUS
+  ENG
+  USA
+  EUR
+Output:Write a SQL query to print a view in such a way that
+  Every team should play with every other opponent exactly once.
+    MATCHES
+  ----------
+  AUS VS ENG
+  AUS VS EUR
+  AUS VS IND
+  AUS VS USA
+  ENG VS EUR
+  ENG VS IND
+  ENG VS USA
+  EUR VS IND
+  EUR VS USA
+  IND VS USA
+
+
+ANS 1:
+    SELECT DISTINCT CASE
+                      WHEN t1.team >= t2.team THEN t2.team
+                      ELSE t1.team
+                    END  || ' VS '||
+                    CASE
+                      WHEN t1.team >= t2.team THEN t1.team
+                      ELSE t2.team
+                    END Matches
+    FROM teams t1
+           cross join teams t2
+    WHERE t1.team != t2.team
+    ORDER BY Matches;
+
+
+ANS 2:
+  SELECT t1.team || ' VS '|| t2.team Matches
+  FROM teams t1
+         cross join teams t2
+         WHERE t1.team < t2.team
+         ORDER BY Matches;
